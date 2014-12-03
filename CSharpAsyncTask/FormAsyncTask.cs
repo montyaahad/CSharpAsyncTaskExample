2d3020db3de5bccd8ed6c6cef28d5d8ead020829
montyaahad <montyaahad@gmail.com>
2014-12-03 21:07:06 +0600

    Async Task Example in CSharp

---
+++ CSharpAsyncTask/FormAsyncTask.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpAsyncTask
+{
+    public partial class FormAyncTask : Form
+    {
+        public FormAyncTask()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAsyncTask_Click(object sender, EventArgs e)
+        {
+            Task<string> task = Task.Factory.StartNew(() => Foo(tbName.Text));
+
+            task.ContinueWith(t => FooResult(task.Result),
+                  TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        private string Foo(string name)
+        {
+            Thread.Sleep(3000);
+
+            return name;
+        }
+
+        private void FooResult(string name)
+        {
+            MessageBox.Show("Wake up, " + name + "!");
+        }
+    }
+}
